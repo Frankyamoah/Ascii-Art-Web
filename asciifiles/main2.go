6728570f99651cee0ab01c64fac9d95e85908eb4
suomar <sumaya.omar404@gmail.com>
2022-11-09 15:39:30 +0000

    progress

--- asciifiles/main2.go
+++ asciifiles/main2.go
@@ -8,14 +8,7 @@ import (
 
 func Asciiart(text, filename string) string {
 
-	letters := strings.Split(text, "")
-
-	//input := os.Args[1:]
-
-	// goes through each character in the input and checks if its outside bounds of
-	// printable character(32-126), if so then print nothing
-	// (this is because runes can range to unicode which has way more characters)
-	for _, word := range letters[0] {
+	for _, word := range text {
 		if word < 32 || word > 126 {
 			return ""
 		}
@@ -30,36 +23,32 @@ func Asciiart(text, filename string) string {
 	// splits text file by new line
 	newAscii := strings.Split(string(bytes), "\n")
 
-	// creates blank array of runes to be appeneded with ascii equivalent of the users input
-	var userInput []rune
-	// create Boolean for new line set to false (\n)
+	var userInput []string
 
-	// loop through each character of user input
-	for _, character := range letters[0] {
+	userInput = append(userInput, text)
 
-		// appends corresponding asciiart characters of letters in users input into blank rune array
-		userInput = append(userInput, character)
-	}
-	// users input and re-formatted standard.txt file passed as aruguments in pritnart function
-
-	return printArtAscii(userInput, newAscii)
+	user3 := strings.Join(userInput, "")
+	return printArtAscii(user3, newAscii)
 }
-func printArtAscii(userInput []rune, Ascii []string) string {
+
+func printArtAscii(userInput string, Ascii []string) string {
 	empty := ""
 	// loops through each row of individual ascii character in art file
 	for line := 1; line <= 8; line++ {
-		// Loop through each character of users input
+
 		for _, character := range userInput {
+
 			//Match users input(rune) with each row of Ascii Art
 			skip := (character - 32) * 9
 			// print the line from art file at the position specified
 			//by calculation of skip to find the corresponding line in to users input
-			empty += Ascii[line+int(skip)]
+			fmt.Print(Ascii[line+int(skip)])
 
 		}
-
+		fmt.Println()
 		// Tells the function to skip to the next line before commencing next loop
 
 	}
 	return empty
+
 }
