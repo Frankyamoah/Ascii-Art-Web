ec1c3ad22996103cdcb6e1ee34a91e9fd02c45f4
suomar <sumaya.omar404@gmail.com>
2022-11-14 13:54:49 +0000

    adding comments

--- asciifiles/main2.go
+++ asciifiles/main2.go
@@ -8,47 +8,51 @@ import (
 
 func Asciiart(text, fontname []string) string {
 
+	//turn the input text into a string
 	text2 := strings.Join(text, "")
-	font2 := strings.Join(fontname, "")
-
+	//range over each character in string to see if printable
 	for _, word := range text2 {
 		if word > 128 {
 			return ""
 		}
 	}
 
-	for _, word := range text2 {
-		if word == '\n' {
-			word += '\n'
-		}
-	}
-
-	// This reads the standard.txt file and checks for error
+	//turn chosen fontname to string
+	font2 := strings.Join(fontname, "")
+	// This reads the file of fontname given and checks for error
 	bytes, err := os.ReadFile("./asciifiles/" + font2 + ".txt")
 	if err != nil {
 		fmt.Println(err)
 		return ""
 	}
 
-	// splits text file by new line
+	//varibale to store the font/banner files after they've been split
 	var newAscii []string
-
+	//have to treat thinkertoy different to standard and shadow
 	if font2 == "thinker-toy" {
+		//for some reason thinkertoy needs to be split by '\r\n' otherwise will print gibberish
 		newAscii = strings.Split(string(bytes), "\r\n")
 
 	} else {
+		//cannot be split by '\r\n' otherwise will print gibberish
 		newAscii = strings.Split(string(bytes), "\n")
 
 	}
+	//to append each character to slice
 	var userInput []string
+
+	//to concatenate with printasciiart function when there is a new line
+	//this is because for every \n encountered the string array will be
+	//wiped to begin the line again without having stored the result before the \n
 	var answer string
 
 	newline := false
 
-	for j := 0; j < len(text); j++ {
+	for i := 0; i < len(text); i++ {
 
-		if text[j] == "\r" {
-			if len(text) != j+1 && text[j+1] == "\n" {
+		//if "\r\n" encountered and not end of string then newline=true and move on
+		if text[i] == "\r" {
+			if len(text) != i+1 && text[i+1] == "\n" {
 				newline = true
 				continue
 
@@ -61,9 +65,11 @@ func Asciiart(text, fontname []string) string {
 			continue
 
 		}
-		userInput = append(userInput, text[j])
+		userInput = append(userInput, text[i])
 
 	}
+	//concatenate answer(outputs that have been stored everytime newline encountered)
+	//with whatever is after the final newline character
 	answer += printArtAscii(userInput, newAscii)
 	return answer
 }
