52610f70eb339996276b198dd64d82b42c24429b
fyamoah <frankyamoah94@gmail.com>
2022-11-14 16:45:26 +0000

    close

--- main.go
+++ main.go
@@ -23,12 +23,19 @@ func main() {
 	http.HandleFunc("/", Index)
 	http.HandleFunc("/process", Processor)
 	http.ListenAndServe(":8080", nil)
-
+	http.HandleFunc("/Error404", Error404)
+	http.HandleFunc("/Error400", Error500)
+	http.HandleFunc("/Error500", Error400)
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		http.ServeFile(w, r, "html/Error404.html")
+		return
+	}
 	tpl.ExecuteTemplate(w, "index.html", nil)
-
+	
 }
 
 func Processor(w http.ResponseWriter, r *http.Request) {
@@ -47,4 +54,17 @@ func Processor(w http.ResponseWriter, r *http.Request) {
 	tpl.ExecuteTemplate(w, "index.html", data)
 	//fmt.Print(asciifiles.Asciiart(textval, fontval))
 
+}
+
+
+func Error404(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "Error404.html", nil)
+}
+
+func Error500(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "Error500.html", nil)
+}
+
+func Error400(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "Error400.html", nil)
 }
